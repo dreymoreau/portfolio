ef6c09d2aa74d00d037af284315fd822fea3b8a2
dreymotoh <striclyspoken@gmail.com>
2023-05-15 17:15:50 -0400

    change

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4
+                border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Omnis sapiente iusto distinctio laboriosam nulla, quos 
+                quam unde et sint reprehenderit voluptate deserunt, nemo 
+                iure dolore. Alias porro architecto mollitia dicta quibusdam 
+                corporis sapiente consequuntur a repellat, nesciunt consequatur 
+                incidunt fugiat suscipit perferendis, atque aperiam eum repudiandae 
+                ad voluptatum? Libero.
+            </p>
+
+            <br />
+
+            
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Omnis sapiente iusto distinctio laboriosam nulla, quos 
+                quam unde et sint reprehenderit voluptate deserunt, nemo 
+                iure dolore. Alias porro architecto mollitia dicta quibusdam 
+                corporis sapiente consequuntur a repellat, nesciunt consequatur 
+                incidunt fugiat suscipit perferendis, atque aperiam eum repudiandae 
+                ad voluptatum? Libero.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
