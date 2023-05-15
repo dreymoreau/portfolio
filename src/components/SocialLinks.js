33da5ce80d5fee66f9f1fc40b1a75207deb42c2c
dreymotoh <striclyspoken@gmail.com>
2023-05-15 17:47:41 -0400

    change

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -56,6 +56,7 @@ const SocialLinks = () => {
                  <li 
                  key={id} 
                  className={
+                    // eslint-disable-next-line no-useless-concat
                     "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
                      <a 
                      href={href} 
