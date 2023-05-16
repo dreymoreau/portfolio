2e4cf3ca89251d0f5592a4cfe65dd91685a5ffd6
dreymotoh <striclyspoken@gmail.com>
2023-05-16 16:52:02 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/Home"
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Home />
       <About />
       <Portfolio />
-      
+      <Experience />
+      <Contact />
+
       <SocialLinks />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroImage from '../assets/heroImage.jpg'
 import {RiArrowRightSLine} from 'react-icons/ri'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -21,14 +22,17 @@ const Home = () => {
           </p>
           
           <div>
-            <button className='group text-white w-fit px-6 py-3 my-2
+            <Link to="portfolio" 
+            smooth 
+            duration={500}
+            className='group text-white w-fit px-6 py-3 my-2
             flex items-center rounded-md bg-gradient-to-r from-cyan-500
             to-blue-500 cursor-pointer'>
               Portfolio 
               <span className='group-hover:rotate-90 duration-300'>
                 <RiArrowRightSLine size={25} className='ml-1'/>
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div> 
